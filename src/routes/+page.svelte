c8287eead1ca4e411fbde77f409e237c7d2164b2
fdumont <fj.dumont@gmail.com>
2023-06-28 17:00:31 +0200

    add landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,30 @@
-Hi I'm landing page
+<div>
+	<input type="text" value="Hund" />
+	<button>Suchen</button>
+</div>
+
+<style>
+	div {
+		min-height: 100vh;
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		gap: var(--size-2);
+	}
+
+	input {
+		border: var(--border-size-2) solid var(--surface-4);
+		background-color: var(--surface-2);
+		width: min(100%, 25rem);
+		padding: var(--size-2) var(--size-4);
+		font-size: var(--font-size-4);
+	}
+
+	button {
+		font-size: var(--font-size-3);
+		padding: var(--size-2) var(--size-4);
+		border-radius: var(--radius-2);
+	}
+</style>
